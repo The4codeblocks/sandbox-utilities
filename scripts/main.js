dd7a2f539f48947d0a089a872a83509dfa89527a
The4codeblocks <the3spaceconstants@gmail.com>
2023-10-06 13:25:17 +0200

    Format fix?

--- scripts/main.js
+++
@@ -1,3 +0,0 @@
-Events.on(EventType.ClientLoadEvent, () => {
-    Vars.mods.locateMod("sandbox-utilities").meta.author = "The Chaoticans\n[sky]T4[stat]SC [gray][Mass <=> [blue]Energy][][][][]";
-});
